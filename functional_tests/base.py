a8e9b079c7b676d7fab39f85b021304a0b045789
juancoquet <juancoquet@gmail.com>
2021-06-07 16:07:56 +0100

    Base FT class setup, ready to start building

---
+++ functional_tests/base.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import os
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+
+class FunctionalTest(StaticLiveServerTestCase):
+
+    def setUp(self):
+        options = Options()
+        options.headless = True
+        self.browser = webdriver.Firefox(options=options)
+        self.browser.get(self.live_server_url)
+
+    def tearDown(self):
+        if self._test_has_failed():
+            for i, handle in enumerate(self.browser.window_handles):
+                self.browser.switch_to.window(handle)
+                filepath = self._create_error_capture_filepath(i)
+                timestamp = datetime.now()
+                self.browser.save_screenshot(f'{filepath}/screenshot-{timestamp}.png')
+                self.capture_html(filepath, timestamp)
+        self.browser.quit()
+
+    def _test_has_failed(self):
+        # returns True if any errors were raised during test run
+        return any(error for (method, error) in self._outcome.errors)
+
+    def _create_error_capture_filepath(self, handle_index):
+        timestamp = datetime.now()
+        classname = self.__class__.__name__
+        method = self._testMethodName
+        dir_path = f'functional_tests/error_capture/{timestamp}-{classname}.{method}/{handle_index}'
+        os.makedirs(dir_path)
+        return dir_path
+
+    def capture_html(self, filepath, timestamp):
+        filename = f'{filepath}/source-{timestamp}.html'
+        with open(filename, 'w') as f:
+            f.write(self.browser.page_source)
---
+++ functional_tests/test_landing_page.py
@@ -0,0 +1,20 @@
+from .base import FunctionalTest
+
+
+class LandingPageTest(FunctionalTest):
+
+    def test_landing_page_invites_new_user(self):
+        # A new unauthenticated user visits the site's homepage.
+        self.browser.get(self.live_server_url)
+
+        # They notice that the page header and title are "Time of Your Life"
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertEqual(header_text, "Time of Your Life")
+        self.assertEqual(self.browser.title, 'Time of Your Life')
+
+        # They are greeted by a brief explanation of what the web app does.
+        description = self.browser.find_element_by_id('site-description')
+        self.assertIn("bird's eye view of your life", description)
+
+        # After reading the explanation, they see a prompt to create a new account to get started.
+        self.assertNotEqual(len(self.browser.find_elements_by_id('new-acc-btn')), 0)
