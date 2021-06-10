55df557d17815b213c02927f55c8f41ee8382174
juancoquet <juancoquet@gmail.com>
2021-06-11 00:09:18 +0100

    Add methods to get current year no. of life and current week no. of year. Needs refactor

--- countdown/forms.py
+++ countdown/forms.py
@@ -1,8 +1,30 @@
 from django import forms
-from datetime import datetime
+from datetime import datetime, date
+import math
 
 
 FUTURE_DOB_ERROR = "Date of birth cannot be in the future"
+
+###############
+### Helpers ###
+###############
+
+
+def is_leap_year(year):
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    else:
+        return False
+
+
+
+#############
+### Forms ###
+#############
 
 
 class DateInput(forms.DateInput):
@@ -19,3 +41,38 @@ class DOBForm(forms.Form):
             raise forms.ValidationError(FUTURE_DOB_ERROR)
         else:
             return dob_given
+
+    def get_current_year_of_life(self):
+        self.full_clean()
+        today = date.today()
+        dob_given = self.cleaned_data['dob']
+        dob_given = date(dob_given.year, dob_given.month, dob_given.day)
+        try:
+            todays_date_on_birth_year = date(dob_given.year, today.month, today.day)
+        except ValueError:  # Today is leap day
+            todays_date_on_birth_year = date(dob_given.year, 3, 1)      # Turn into 1st March
+        if dob_given > todays_date_on_birth_year:
+            current_year = today.year - dob_given.year
+        else:
+            current_year = (today.year - dob_given.year) + 1
+        return current_year
+
+    def get_current_week_no(self):
+        self.full_clean()
+        today = date.today()
+        dob_given = self.cleaned_data['dob']
+        dob_given = date(dob_given.year, dob_given.month, dob_given.day)
+
+        try:
+            todays_date_on_birth_year = date(dob_given.year, today.month, today.day)
+        except ValueError:  # Today is leap day
+            todays_date_on_birth_year = date(dob_given.year, 3, 1)  # Turn into 1st March
+
+        if dob_given > todays_date_on_birth_year:
+            try:
+                todays_date_on_birth_year = date(dob_given.year+1, today.month, today.day)
+            except ValueError:  # Today is leap day
+                todays_date_on_birth_year = date(dob_given.year+1, 3, 1)  # Turn into 1st March
+        days_since_bday = (todays_date_on_birth_year - dob_given).days
+        return math.ceil(days_since_bday / 7)
+
--- countdown/tests/test_forms.py
+++ countdown/tests/test_forms.py
@@ -1,6 +1,9 @@
+import datetime
 from django.test import TestCase
+from unittest.mock import patch
 
-from countdown.forms import DOBForm, FUTURE_DOB_ERROR
+from countdown import forms
+from countdown.forms import DOBForm, FUTURE_DOB_ERROR, is_leap_year
 
 
 class DOBFormTest(TestCase):
@@ -9,3 +12,71 @@ class DOBFormTest(TestCase):
         form = DOBForm(data={'dob': '2999-12-31'})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['dob'], [FUTURE_DOB_ERROR])
+
+    def test_past_date_is_valid(self):
+        form = DOBForm(data={'dob': '1995-12-01'})
+        self.assertTrue(form.is_valid())
+
+    def test_get_current_year_of_life(self):
+
+        class MockToday(datetime.date):
+            @classmethod
+            def today(cls):
+                return cls(2021, 6, 9)
+
+        forms.date = MockToday
+        dob = DOBForm(data={'dob': '1995-12-01'})
+        self.assertEqual(dob.get_current_year_of_life(), 26)
+
+    def test_get_current_year_of_life_if_today_is_leap_day(self):
+
+        class MockToday(datetime.date):
+            @classmethod
+            def today(cls):
+                return cls(2020, 2, 29)
+
+        forms.date = MockToday
+
+        dob = DOBForm(data={'dob': '2000-02-29'})
+        self.assertEqual(dob.get_current_year_of_life(), 21)
+
+    def test_get_current_week_no(self):
+
+        class MockToday(datetime.date):
+            @classmethod
+            def today(cls):
+                return cls(2021, 6, 9)
+
+        forms.date = MockToday
+
+        dob = DOBForm(data={'dob': '1995-12-01'})
+        self.assertEqual(dob.get_current_week_no(), 28)
+        dob = DOBForm(data={'dob': '2004-02-29'})
+        self.assertEqual(dob.get_current_week_no(), 15)
+
+    def test_get_current_week_no_if_today_is_leap_day(self):
+
+        class MockToday(datetime.date):
+            @classmethod
+            def today(cls):
+                return cls(2020, 2, 29)
+
+        forms.date = MockToday
+
+        dob = DOBForm(data={'dob': '1995-12-01'})
+        self.assertEqual(dob.get_current_week_no(), 13)
+        dob = DOBForm(data={'dob': '1996-12-01'})
+        self.assertEqual(dob.get_current_week_no(), 13)
+
+
+class HelperTest(TestCase):
+
+    def test_is_leap_year(self):
+        self.assertTrue(is_leap_year(1916))
+        self.assertTrue(is_leap_year(2024))
+        self.assertTrue(is_leap_year(2000))
+        self.assertTrue(is_leap_year(2400))
+        self.assertFalse(is_leap_year(1917))
+        self.assertFalse(is_leap_year(2025))
+        self.assertFalse(is_leap_year(2100))
+
