96ef936d86875800a1d679de14bfa8d0f8e6dd25
juancoquet <juancoquet@gmail.com>
2021-07-10 22:51:41 +0100

    User forms testing

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,11 @@
+from countdown.forms import FUTURE_DOB_ERROR
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.forms import ValidationError
+from django.utils import timezone
+from datetime import date
+
+from countdown.forms import FUTURE_DOB_ERROR, PAST_DOB_ERROR, get_today_minus_90_years
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -8,9 +14,27 @@ class CustomUserCreationForm(UserCreationForm):
         model = get_user_model()
         fields = ('username', 'email', 'first_name', 'dob',)
 
+    def clean_dob(self, *args, **kwargs):
+        dob_given = self.cleaned_data['dob']
+        if dob_given >= timezone.now().date():
+            raise ValidationError(FUTURE_DOB_ERROR)
+        if dob_given < get_today_minus_90_years().date():
+            raise ValidationError(PAST_DOB_ERROR)
+        else:
+            return dob_given
+
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = get_user_model()
         fields = ('username', 'email', 'first_name', 'dob',)
+
+    def clean_dob(self, *args, **kwargs):
+        dob_given = self.cleaned_data['dob']
+        if dob_given >= timezone.now().date():
+            raise ValidationError(FUTURE_DOB_ERROR)
+        if dob_given < get_today_minus_90_years().date():
+            raise ValidationError(PAST_DOB_ERROR)
+        else:
+            return dob_given
--- accounts/tests/test_forms.py
+++ accounts/tests/test_forms.py
@@ -1,1 +1,45 @@
-# TODO: Test custom user forms
+from datetime import date
+from django.test import TestCase
+
+from accounts.forms import CustomUserCreationForm, CustomUserChangeForm, FUTURE_DOB_ERROR, PAST_DOB_ERROR
+
+
+class CreationFormTest(TestCase):
+
+    def test_valid_user_form(self):
+        form = CustomUserCreationForm(
+            data={
+                'username': 'juan',
+                'email': 'test@email.com',
+                'password1': 'testpass123',
+                'password2': 'testpass123',
+                'dob': '1995-12-01'
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_cannot_have_future_dob(self):
+        form = CustomUserCreationForm(
+            data={
+                'username': 'juan',
+                'email': 'test@email.com',
+                'password1': 'testpass123',
+                'password2': 'testpass23',
+                'dob': '2100-12-01'
+            }
+        )
+        form.full_clean()
+        self.assertEqual(form.errors['dob'], [FUTURE_DOB_ERROR])
+
+    def test_dob_more_than_90_years_ago_is_not_valid(self):
+        form = CustomUserCreationForm(
+            data={
+                'username': 'juan',
+                'email': 'test@email.com',
+                'password1': 'testpass123',
+                'password2': 'testpass23',
+                'dob': '1900-12-01'
+            }
+        )
+        form.full_clean()
+        self.assertEqual(form.errors['dob'], [PAST_DOB_ERROR])
