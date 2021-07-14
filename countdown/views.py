74e43535050447e81fbdc1eff8229619c201ab7d
juancoquet <juancoquet@gmail.com>
2021-07-14 11:52:29 +0100

    Refactor countdown views helpers into separate file

--- countdown/views.py
+++ countdown/views.py
@@ -1,68 +1,9 @@
-import math
-
-from datetime import date, datetime
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from .forms import DOBForm, EVENT_DATE_ERROR, EventForm
-
-
-###############
-### Helpers ###
-###############
-
-def get_event_year_of_life(event_date, dob):
-    try:
-        event_date_on_birth_year = date(
-            dob.year, event_date.month, event_date.day)
-    except ValueError:  # Event day is leap day
-        event_date_on_birth_year = date(dob.year, 3, 1)  # Turn into 1st March
-
-    if event_date_on_birth_year < dob:
-        event_year_of_life = event_date.year - dob.year
-    else:
-        event_year_of_life = (event_date.year - dob.year) + 1
-
-    return event_year_of_life
-
-
-def get_event_week_number(event_date, dob):
-    try:
-        event_date_on_birth_year = date(
-            dob.year, event_date.month, event_date.day)
-    except ValueError:  # Event day is leap day
-        event_date_on_birth_year = date(dob.year, 3, 1)  # Turn into 1st March
-    if event_date_on_birth_year < dob:
-        event_date_on_birth_year = date(
-            event_date_on_birth_year.year+1,
-            event_date_on_birth_year.month,
-            event_date_on_birth_year.day
-        )
-
-    days_passed = (event_date_on_birth_year - dob).days
-    week_no = math.ceil(days_passed / 7)
-    if week_no == 53:
-        week_no = 52
-    if week_no == 0:
-        week_no = 1
-    return week_no
-
-
-def event_is_within_90_yrs_of_dob(event_date, dob):
-    try:
-        dob_plus_90 = date(dob.year+90, dob.month, dob.day)
-    except ValueError:  # dob is leap day
-        dob_plus_90 = date(dob.year+90, 3, 1)  # Turn into 1st March
-    if event_date > dob_plus_90 or event_date < dob:
-        return False
-    else:
-        return True
-
-
-#############
-### Views ###
-#############
+from .view_helpers import event_is_within_90_yrs_of_dob, get_event_year_of_life, get_event_week_number
 
 
 def home(request):
