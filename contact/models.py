74ee7955dcea1209e31df7bf045def0ad17540cb
juancoquet <juancoquet@gmail.com>
2021-07-30 13:24:12 +0100

    Change contact to sendinblue

--- contact/models.py
+++ contact/models.py
@@ -4,7 +4,9 @@ import smtplib
 from email.message import EmailMessage
 
 from accounts.models import CustomUser
-from timeofyourlife_v1.my_secrets import GMAIL, GMAIL_PASS
+from timeofyourlife_v1.my_secrets import (GMAIL, GMAIL_PASS, MY_EMAIL_HOST,
+                                          MY_EMAIL_HOST, MY_EMAIL_HOST_USER,
+                                          MY_EMAIL_HOST_PASSWORD, MY_EMAIL_PORT)
 
 
 class Feedback(models.Model):
@@ -29,13 +31,13 @@ class Feedback(models.Model):
     def send_notification(self):
         msg = EmailMessage()
         msg['Subject'] = self.subject if self.subject else 'No subject'
-        msg['From'] = 'Time of Your Life <noreply@timeofyourlife.io'
+        msg['From'] = 'Time of Your Life <noreply@timeofyourlife.io>'
         msg['To'] = GMAIL
         msg.set_content(
             f'{self.message}\n\n\nemail: {self.email}\nuser: {self.user}')
 
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-            smtp.login(GMAIL, GMAIL_PASS)
+        with smtplib.SMTP(MY_EMAIL_HOST, MY_EMAIL_PORT) as smtp:
+            smtp.login(MY_EMAIL_HOST_USER, MY_EMAIL_HOST_PASSWORD)
             smtp.send_message(msg)
 
     def save(self, *args, **kwargs) -> None:
