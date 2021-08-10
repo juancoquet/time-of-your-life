de9c88ea91799cd5ac9eb074f01687d2c9cdce08
juancoquet <juancoquet@gmail.com>
2021-08-10 16:18:02 +0100

    Style dashboard calendar

--- accounts/models.py
+++ accounts/models.py
@@ -115,7 +115,7 @@ class CustomUser(AbstractUser):
 
     @property
     def calendar(self):
-        html = '<table>'
+        html = '<div class="calendar">'
         events = {}
         for event in self.events.all():
             if not events.get(str(event.index)):
@@ -124,15 +124,15 @@ class CustomUser(AbstractUser):
                 events[str(event.index)].append(event)
 
         for year in range(1, 91):
-            html += f'<tr class="year" id="year-{year}">'
+            html += f'<div class="year-row" id="year-{year}">'
             weeks_in_yr = {}
             for week in range(1, 53):
                 if (year, week) < self.current_year_and_week:
-                    weeks_in_yr[f'({year}, {week})'] = f'<td class="week past" id="({year},{week})">{week}</td>'
+                    weeks_in_yr[f'({year}, {week})'] = f'<div class="week past" id="({year},{week})"><p class="week__number">{week}</p></div>'
                 elif (year, week) > self.current_year_and_week:
-                    weeks_in_yr[f'({year}, {week})'] = f'<td class="week future" id="({year},{week})">{week}</td>'
+                    weeks_in_yr[f'({year}, {week})'] = f'<div class="week future" id="({year},{week})"><p class="week__number">{week}</p></div>'
                 else:
-                    weeks_in_yr[f'({year}, {week})'] = f'<td class="week present" id="({year},{week})">{week}</td>'
+                    weeks_in_yr[f'({year}, {week})'] = f'<div class="week present" id="({year},{week})"><p class="week__number">{week}</p></div>'
 
             for wk_index, event_iter in events.items():
                 if week_element := weeks_in_yr.get(wk_index):
@@ -140,8 +140,8 @@ class CustomUser(AbstractUser):
                         '"', 2)
                     classes += " event"
                     with_classes = f'{up_to_class}"{classes}" {id_onwards}'
-                    wihtout_closing_tag, _ = with_classes.split('</')
-                    before_event_details = f'{wihtout_closing_tag}<div class="all-tooltips"><div class="tooltip">\
+
+                    before_event_details = f'<div class="all-tooltips">{with_classes}<div class="tooltip">\
                         <div class="tooltip-content"><div class="arrow"></div><div class="content">'
                     event_details = ''
                     after_event_details = '</div></div></div></div>'
@@ -152,14 +152,14 @@ class CustomUser(AbstractUser):
                         event_url = event.get_edit_url()
                         event_delete_url = event.get_delete_url()
                         event_details += f'<div class="event_details"><h3>{event_name}</h3><p>{event_date}</p>\
-                                <p><span class="edit"><a href="{event_url}">Edit</a></span>\
-                                <span class="delete"><a href="{event_delete_url}">Delete</a></span></p></div>'
+                                <p><a class="tooltip__link" href="{event_url}">Edit</a>\
+                                <a class="tooltip__link" href="{event_delete_url}">Delete</a></p></div>'
 
                     new_element = before_event_details + event_details + after_event_details
                     weeks_in_yr[str(event.index)] = new_element
 
             for _, value in weeks_in_yr.items():
                 html += value
-            html += '</tr>'
-        html += '</table>'
+            html += '</div>'
+        html += '</div>'
         return html
