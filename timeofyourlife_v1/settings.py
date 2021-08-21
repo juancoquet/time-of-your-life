901116d09ae8e81c3fa42184280c06974b5dd50c
juancoquet <juancoquet@gmail.com>
2021-08-21 13:14:00 +0100

    Add environment variables

--- timeofyourlife_v1/settings.py
+++ timeofyourlife_v1/settings.py
@@ -10,8 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
-import os
 from .my_secrets import (
     DJANGO_SECRET_KEY,
     MY_EMAIL_HOST,
@@ -19,6 +17,14 @@ from .my_secrets import (
     MY_EMAIL_HOST_PASSWORD,
     MY_EMAIL_PORT,
 )
+from pathlib import Path
+import os
+
+from environs import Env
+
+env = Env()
+env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,9 +37,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -112,14 +118,8 @@ WSGI_APPLICATION = 'timeofyourlife_v1.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432
-    }
+    'default': env.dj_db_url("DATABASE_URL",
+                             default="postgres://postgres@db/postgres")
 }
 
 
