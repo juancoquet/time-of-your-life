66f48c2b8b4a8dc4ac6b40022e54c9d5fe4a87d1
juancoquet <juancoquet@gmail.com>
2021-08-26 21:17:09 +0100

    Calculate tooltip positoin on tap/hover

--- static/js/tooltips.js
+++ static/js/tooltips.js
@@ -6,6 +6,8 @@ window.addEventListener('DOMContentLoaded', contentPosition);
 window.addEventListener('resize', contentPosition);
 // window.addEventListener('DOMContentLoaded', buildModal);
 window.addEventListener('DOMContentLoaded', tapPresentWeek);
+window.addEventListener('DOMContentLoaded', calculatePositionOnTap);
+window.addEventListener('DOMContentLoaded', calculatePositionOnHover);
 
 
 function contentPosition(){
@@ -41,6 +43,78 @@ function contentPosition(){
             content.style.top = -content.offsetHeight/2 + event.offsetHeight/2 + 'px';
         }
         
+    })
+}
+
+function calculatePositionOnTap() {
+    tooltips.forEach(tooltip => {
+        tooltip.addEventListener('touchstart', function calculate() {
+            console.log('calculating');
+
+            const event = tooltip.querySelector('.week.event');
+            const content = tooltip.querySelector('.tooltip-content');
+            const arrow = tooltip.querySelector('.arrow');
+            
+            tooltipBoundaries = content.getBoundingClientRect();
+            calendarBoundaies = calendar.getBoundingClientRect();
+            console.log(tooltipBoundaries)
+
+            if(tooltipBoundaries.right > calendarBoundaies.right){
+                arrow.style.borderBottomColor = 'transparent';
+                arrow.style.borderLeftColor = '#F8F1F1';
+                arrow.style.top = arrow.offsetHeight/2 + content.offsetHeight/2 + 'px';
+                arrow.style.left = content.offsetWidth + arrow.offsetWidth/2 + 'px';
+                
+                content.style.left = -content.offsetWidth - arrow.offsetWidth + 'px';
+                content.style.top = -content.offsetHeight/2 + event.offsetHeight/2 + 'px';
+            }
+
+            if(tooltipBoundaries.left < calendarBoundaies.left){
+                arrow.style.borderBottomColor = 'transparent';
+                arrow.style.borderRightColor = '#F8F1F1';
+                arrow.style.top = arrow.offsetHeight/2 + content.offsetHeight/2 + 'px';
+                arrow.style.left = -arrow.offsetWidth/2 + 'px';
+                
+                content.style.left = event.offsetWidth + arrow.offsetWidth + 'px';
+                content.style.top = -content.offsetHeight/2 + event.offsetHeight/2 + 'px';
+            }
+        })
+    })
+}
+
+function calculatePositionOnHover() {
+    tooltips.forEach(tooltip => {
+        tooltip.addEventListener('mouseenter', function calculate() {
+            console.log('calculating');
+
+            const event = tooltip.querySelector('.week.event');
+            const content = tooltip.querySelector('.tooltip-content');
+            const arrow = tooltip.querySelector('.arrow');
+            
+            tooltipBoundaries = content.getBoundingClientRect();
+            calendarBoundaies = calendar.getBoundingClientRect();
+            console.log(tooltipBoundaries)
+
+            if(tooltipBoundaries.right > calendarBoundaies.right){
+                arrow.style.borderBottomColor = 'transparent';
+                arrow.style.borderLeftColor = '#F8F1F1';
+                arrow.style.top = arrow.offsetHeight/2 + content.offsetHeight/2 + 'px';
+                arrow.style.left = content.offsetWidth + arrow.offsetWidth/2 + 'px';
+                
+                content.style.left = -content.offsetWidth - arrow.offsetWidth + 'px';
+                content.style.top = -content.offsetHeight/2 + event.offsetHeight/2 + 'px';
+            }
+
+            if(tooltipBoundaries.left < calendarBoundaies.left){
+                arrow.style.borderBottomColor = 'transparent';
+                arrow.style.borderRightColor = '#F8F1F1';
+                arrow.style.top = arrow.offsetHeight/2 + content.offsetHeight/2 + 'px';
+                arrow.style.left = -arrow.offsetWidth/2 + 'px';
+                
+                content.style.left = event.offsetWidth + arrow.offsetWidth + 'px';
+                content.style.top = -content.offsetHeight/2 + event.offsetHeight/2 + 'px';
+            }
+        })
     })
 }
 
